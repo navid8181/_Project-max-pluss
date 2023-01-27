fb90d8b9fd1b0c8e7a119d1f647a08d41be19930
Pouya Hezaveh <107344017+Hezavehir@users.noreply.github.com>
2023-01-27 23:43:10 +0330

    putting Basic RAM (RAM without clock) in the computer design

---
+++ Basic_RAM_inst.v
@@ -0,0 +1,7 @@
+Basic_RAM	Basic_RAM_inst (
+	.data ( data_sig ),
+	.wraddress ( wraddress_sig ),
+	.rdaddress ( rdaddress_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
---
+++ Tiny_Basic_Ram_inst.v
@@ -0,0 +1,7 @@
+Tiny_Basic_Ram	Tiny_Basic_Ram_inst (
+	.data ( data_sig ),
+	.wraddress ( wraddress_sig ),
+	.rdaddress ( rdaddress_sig ),
+	.wren ( wren_sig ),
+	.q ( q_sig )
+	);
